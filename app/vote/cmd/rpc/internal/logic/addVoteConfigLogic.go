8663be7c25c0e56e4c2e13b93540470a8c2138e9
mike <coolfeng2008@outlook.com>
2024-01-25 16:57:55 +0800

    投票: 添加创建配置rpc功能

--- app/vote/cmd/rpc/internal/logic/addVoteConfigLogic.go
+++ app/vote/cmd/rpc/internal/logic/addVoteConfigLogic.go
@@ -2,6 +2,10 @@ package logic
 
 import (
 	"context"
+	"github.com/jinzhu/copier"
+	"github.com/pkg/errors"
+	"looklook/app/vote/model"
+	"looklook/common/xerr"
 
 	"looklook/app/vote/cmd/rpc/internal/svc"
 	"looklook/app/vote/cmd/rpc/pb"
@@ -25,7 +29,22 @@ func NewAddVoteConfigLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Add
 
 // -----------------------投票表-----------------------
 func (l *AddVoteConfigLogic) AddVoteConfig(in *pb.AddVoteConfigReq) (*pb.AddVoteConfigResp, error) {
-	// todo: add your logic here and delete this line
+	voteConfig := new(model.VoteConfig)
+	err := copier.Copy(voteConfig, in)
+	if err != nil {
+		return nil, errors.Wrapf(xerr.NewErrCode(xerr.DB_ERROR), "copier : %+v , err: %v", in, err)
+	}
 
-	return &pb.AddVoteConfigResp{}, nil
+	insertResult, err := l.svcCtx.VoteConfigModel.Insert(l.ctx, voteConfig)
+	if err != nil {
+		return nil, errors.Wrapf(xerr.NewErrCode(xerr.DB_ERROR), "Add address db user_address Insert err:%v, address:%+v", err, voteConfig)
+	}
+	lastId, err := insertResult.LastInsertId()
+	if err != nil {
+		return nil, errors.Wrapf(xerr.NewErrCode(xerr.DB_ERROR), "Add address db user_address insertResult.LastInsertId err:%v, address:%+v", err, voteConfig)
+	}
+
+	return &pb.AddVoteConfigResp{
+		Id: lastId,
+	}, nil
 }
--- app/vote/cmd/rpc/internal/svc/serviceContext.go
+++ app/vote/cmd/rpc/internal/svc/serviceContext.go
@@ -1,13 +1,30 @@
 package svc
 
-import "looklook/app/vote/cmd/rpc/internal/config"
+import (
+	"github.com/zeromicro/go-zero/core/stores/redis"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+	"looklook/app/vote/cmd/rpc/internal/config"
+	"looklook/app/vote/model"
+)
 
 type ServiceContext struct {
-	Config config.Config
+	Config      config.Config
+	RedisClient *redis.Redis
+
+	VoteConfigModel model.VoteConfigModel
+	VoteRecord      model.VoteRecordModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	sqlConn := sqlx.NewMysql(c.DB.DataSource)
+
 	return &ServiceContext{
 		Config: c,
+		RedisClient: redis.New(c.Redis.Host, func(r *redis.Redis) {
+			r.Type = c.Redis.Type
+			r.Pass = c.Redis.Pass
+		}),
+
+		VoteConfigModel: model.NewVoteConfigModel(sqlConn, c.Cache),
 	}
 }
