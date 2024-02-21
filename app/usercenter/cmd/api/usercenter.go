19af090fa6a038e78a4f7c0ed92afbb00698d12f
antony王中阳 <425772719@qq.com>
2024-02-21 21:09:23 +0800

    debug

--- app/usercenter/cmd/api/usercenter.go
+++ app/usercenter/cmd/api/usercenter.go
@@ -18,7 +18,8 @@ func main() {
 	var c config.Config
 	conf.MustLoad(*configFile, &c, conf.UseEnv())
 	//设置允许跨域访问
-	server := rest.MustNewServer(c.RestConf, rest.WithCors())
+	//server := rest.MustNewServer(c.RestConf, rest.WithCors())
+	server := rest.MustNewServer(c.RestConf)
 	defer server.Stop()
 
 	ctx := svc.NewServiceContext(c)
