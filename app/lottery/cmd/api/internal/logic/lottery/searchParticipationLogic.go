d9c659d48994625037f0b2f414db155c2c66d86b
roy <1842568012@qq.com>
2024-03-05 23:39:57 +0800

    fix(lottery): 获取抽奖人列表

--- app/lottery/cmd/api/internal/logic/lottery/searchParticipationLogic.go
+++ app/lottery/cmd/api/internal/logic/lottery/searchParticipationLogic.go
@@ -3,13 +3,11 @@ package lottery
 import (
 	"context"
 	"github.com/jinzhu/copier"
-	"looklook/app/lottery/cmd/rpc/lottery"
-	"looklook/app/usercenter/cmd/rpc/usercenter"
-
+	"github.com/zeromicro/go-zero/core/logx"
 	"looklook/app/lottery/cmd/api/internal/svc"
 	"looklook/app/lottery/cmd/api/internal/types"
-
-	"github.com/zeromicro/go-zero/core/logx"
+	"looklook/app/lottery/cmd/rpc/lottery"
+	"looklook/app/usercenter/cmd/rpc/usercenter"
 )
 
 type SearchParticipationLogic struct {
@@ -29,22 +27,27 @@ func NewSearchParticipationLogic(ctx context.Context, svcCtx *svc.ServiceContext
 func (l *SearchParticipationLogic) SearchParticipation(req *types.SearchLotteryParticipationReq) (resp *types.SearchLotteryParticipationResp, err error) {
 	r, err := l.svcCtx.LotteryRpc.SearchLotteryParticipation(l.ctx, &lottery.SearchLotteryParticipationReq{
 		LotteryId: req.LotteryId,
+		PageIndex: req.PageIndex,
+		PageSize:  req.PageSize,
 	})
 	if err != nil {
 		return nil, err
 	}
 
-	userInfos := []*usercenter.User{}
+	var userIds []int64
+
 	for i := range r.List {
-		userId := r.List[i].UserId
-		info, err := l.svcCtx.UsercenterRpc.GetUserInfo(l.ctx, &usercenter.GetUserInfoReq{
-			Id: userId,
-		})
-		if err != nil {
-			return nil, err
-		}
-		userInfos = append(userInfos, info.User)
+		userIds = append(userIds, r.List[i].UserId)
 	}
-	err = copier.Copy(&resp.List, userInfos)
+
+	userInfos, err := l.svcCtx.UsercenterRpc.GetUserInfoByUserIds(l.ctx, &usercenter.GetUserInfoByUserIdsReq{
+		UserIds: userIds,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	resp = new(types.SearchLotteryParticipationResp)
+	err = copier.Copy(&resp.List, userInfos.UserInfo)
 	return
 }
