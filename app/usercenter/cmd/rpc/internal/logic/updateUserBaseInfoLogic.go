256fff300df1428a502b58669822e6a51233b7aa
coderon <1710034886@qq.com>
2024-08-23 05:20:01 -0400

    [fix] split big transiaction in UpdateUserBaseInfo

--- app/usercenter/cmd/rpc/internal/logic/updateUserBaseInfoLogic.go
+++ app/usercenter/cmd/rpc/internal/logic/updateUserBaseInfoLogic.go
@@ -2,10 +2,6 @@ package logic
 
 import (
 	"context"
-	"github.com/pkg/errors"
-	"github.com/zeromicro/go-zero/core/stores/sqlx"
-	"looklook/common/xerr"
-
 	"looklook/app/usercenter/cmd/rpc/internal/svc"
 	"looklook/app/usercenter/cmd/rpc/pb"
 
@@ -28,26 +24,21 @@ func NewUpdateUserBaseInfoLogic(ctx context.Context, svcCtx *svc.ServiceContext)
 
 func (l *UpdateUserBaseInfoLogic) UpdateUserBaseInfo(in *pb.UpdateUserBaseInfoReq) (*pb.UpdateUserBaseInfoResp, error) {
 	// todo: 暂时没有不带session的update 使用事务写入
-	err := l.svcCtx.UserModel.Trans(l.ctx, func(context context.Context, session sqlx.Session) error {
-		user, err := l.svcCtx.UserModel.FindOne(l.ctx, in.Id)
-		if err != nil {
-			return errors.Wrapf(xerr.NewErrCode(xerr.DB_ERROR), "user not exist err:%v,user:%+v", err, user)
-		}
-		user.Nickname = in.Nickname
-		user.Sex = in.Sex
-		user.Info = in.Info
-		user.Avatar = in.Avatar
-		user.Signature = in.Signature
-		user.Longitude = in.Longitude
-		user.Latitude = in.Latitude
-		err = l.svcCtx.UserModel.Update(context, user)
-		if err != nil {
-			return errors.Wrapf(xerr.NewErrCode(xerr.DB_ERROR), "update user base info err:%v,user:%+v", err, user)
-		}
-		return nil
-	})
+
+	user, err := l.svcCtx.UserModel.FindOne(l.ctx, in.Id)
 	if err != nil {
-		logx.Error("update user base info err:", err)
+		return nil, err
+	}
+	user.Nickname = in.Nickname
+	user.Sex = in.Sex
+	user.Info = in.Info
+	user.Avatar = in.Avatar
+	user.Signature = in.Signature
+	user.Longitude = in.Longitude
+	user.Latitude = in.Latitude
+
+	err = l.svcCtx.UserModel.Update(l.ctx, user)
+	if err != nil {
 		return nil, err
 	}
 	return &pb.UpdateUserBaseInfoResp{}, nil
